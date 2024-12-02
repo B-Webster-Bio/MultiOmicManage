951a11d46e2bc5a89f92707b5a65fbcd1e0f5748
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-12-02 18:34:30 -0500

    Update Landing.py

--- Landing.py
+++ Landing.py
@@ -13,4 +13,7 @@ with col1:
     st.image(r'Supp/CornHarvest.PNG', caption = 'Hand corn harvest (PBS Wisconsin)')
 
 with col2:
-    st.markdown("KernelDryWt_PerPlant -  Avg grams of grain per plant")
+    st.markdown('Agronomic traits were collected by hand. They include some of the most important trait that a plant breeding might consider')
+    st.markdown("**KernelDryWt_PerPlant** -  Avg grams of grain per plant harvested")
+    st.markdown('**DaysToSilk** - The number of days after planting it takes for flowering to occur')
+    st.markdown('**AvgFlagHt** - The height measured from the ground to the top leaf')
