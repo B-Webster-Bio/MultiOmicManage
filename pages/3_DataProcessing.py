1e5f34445e34e7f3c25aded1135e473ea4239cd0
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-12-02 21:39:30 -0500

    DataProcess

--- pages/3_DataProcessing.py
+++ pages/3_DataProcessing.py
@@ -1,114 +1,132 @@
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import zscore
 import numpy as np
 
-# multispectral reflectance reflectance
-df_ref = pd.read_csv('Raw/MultiSpec.csv')
-# meta data about field plots
-df_meta = pd.read_csv('Raw/MasterMetaData.csv')
+# Streamlit app title
+st.title("Data Processing")
+
+
+# Load datasets
+st.header("Load Data")
+try:
+    df_ref = pd.read_csv('Raw/MultiSpec.csv')
+    df_meta = pd.read_csv('Raw/MasterMetaData.csv')
+    df_agro = pd.read_csv('Raw/MasterAgronData.csv')
+    df_gasex = pd.read_csv('Raw/MasterGasExData.csv')
+    st.success("Datasets loaded successfully!")
+except Exception as e:
+    st.error(f"Error loading data: {e}")
+    st.stop()
+
+# Data preprocessing
 df_meta.drop(columns=['FTREATMENT', 'TYPE', 'LOCATION', 'PLANTINGDATE'], inplace=True)
-# hand collected agronomy data
-df_agro = pd.read_csv('Raw/MasterAgronData.csv')
-# gas excahnge data from Licor
-df_gasex = pd.read_csv('Raw/MasterGasExData.csv')
 df_gasex.drop(columns=['date', 'time'], inplace=True)
 
-# formatting
+# Formatting
 dfs = [df_ref, df_agro, df_meta, df_gasex]
 for df in dfs:
     df['YEAR'] = df['YEAR'].astype('str')
     df['PLOT'] = df['PLOT'].astype('str')
     df['PLOT_YEAR'] = df['PLOT_YEAR'].astype('str')
 
-# Subset those with gas_ex measurements
-df_gasex = pd.merge(df_meta, df_gasex, on = ['PLOT_YEAR', 'PLOT', 'YEAR'], how='inner')
+# Merge
+df_gasex = pd.merge(df_meta, df_gasex, on=['PLOT_YEAR', 'PLOT', 'YEAR'], how='inner')
 
-# before aggregating
-plt.figure(figsize=(8, 5))
-sns.heatmap(df_gasex.isna(), cmap="magma")
-plt.title('Missing Before Aggregate')
+st.subheader('Gas Exchange Data:')
+st.markdown('Gas Exchange is collected twice per plot so we will take average on the plot level.')
+# Visualize missing data
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.heatmap(df_gasex.isna(), cmap="magma", ax=ax)
+plt.title("Missing Data Before Mean Aggregation")
+st.pyplot(fig)
 
-# list of cols to calc mean
-cols = list(df_gasex.columns[9:])
-cols.append('PLOT_YEAR')
-df_gasex_g = df_gasex[cols]
-# calc mean based on unique PLOT_YEAR
-df_gasex_g = df_gasex_g.groupby(by='PLOT_YEAR').mean().reset_index()
+# Aggregation and merging
+cols = list(df_gasex.columns[9:]) + ['PLOT_YEAR']
+df_gasex_g = df_gasex[cols].groupby(by='PLOT_YEAR').mean().reset_index()
+df_gasex_g = pd.merge(df_meta, df_gasex_g, on='PLOT_YEAR', how='inner')
 
-# Merge it back with meta data
-df_gasex_g = pd.merge(df_meta, df_gasex_g, on = 'PLOT_YEAR', how='inner')
-print('Total plot samples before: {}'.format(len(df_gasex_g.index)))
-
-# plot after 
-plt.figure(figsize=(8, 5))
-sns.heatmap(df_gasex_g.isna(), cmap="magma")
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.heatmap(df_gasex_g.isna(), cmap="magma", ax=ax)
 plt.title('Missing After Aggregate')
+st.pyplot(fig)
 
-# taking mean of plot_level resolved all missing except for one.
-print(df_gasex_g.loc[df_gasex_g['A'].isna(),:])
-# Not sure why this sample is missing let's look at yield and flowering
-
-# before fill impution
-# sort by year to see MAR
+# Imputation
+st.subheader('Missing Meta Data')
+st.markdown('Meta data from one year was not listed but we can use the fwd fill method to fill in missing entries with similar ones.')
 df_gasex_g.sort_values(by='YEAR', inplace=True)
-plt.figure(figsize=(8, 5))
-sns.heatmap(df_gasex_g.isna(), cmap="magma", vmin=0, vmax = 1)
-plt.title('Missing Before Fill')
+df_gasex_g['SUBPOPULATION'] = df_gasex_g.groupby('GENOTYPE')['SUBPOPULATION'].transform(
+    lambda x: x.fillna(method='ffill').fillna(method='bfill'))
 
-# impute missing
-df_gasex_g['SUBPOPULATION'] = df_gasex_g.groupby('GENOTYPE')['SUBPOPULATION'].transform(lambda x: x.fillna(method='ffill').fillna(method='bfill'))
-
-# after 
-plt.figure(figsize=(8, 5))
-sns.heatmap(df_gasex_g.isna(), cmap="magma", vmin=0, vmax = 1)
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.heatmap(df_gasex_g.isna(), cmap="magma", ax=ax)
 plt.title('Missing After Fill')
+st.pyplot(fig)
+
 
 # keys for merging
 keys = ['PLOT_YEAR', 'PLOT', 'YEAR']
-
 # merge agronomic traits of interest with gas ex
-cols2keep = ['KERNELDRYWT_PERPLANT', 'KERNELMOISTURE_P', 'DAYSTOANTHESIS',
-              'DAYSTOSILK', 'ASI', 'AVGFLAGHT_CM'] + keys
-
+cols2keep = ['KERNELDRYWT_PERPLANT',
+              'DAYSTOSILK', 'AVGFLAGHT_CM'] + keys
 df_agro = df_agro.loc[:, cols2keep]
-
 df_gasex_g = pd.merge(df_gasex_g, df_agro, on=keys, how='inner')
-
-# all of the missing values are from 2023
-df_gas_miss = df_gasex_g.loc[df_gasex_g['KERNELDRYWT_PERPLANT'].isna(), :]
-print(df_gas_miss['YEAR'].value_counts())
-
-df_gasex_g.sort_values(by='YEAR', inplace=True)
-plt.figure(figsize=(8,5))
-sns.heatmap(df_gasex_g.isna(), cmap="magma")
 
 # Assess if the yield missingness is related to any other variable
 df_gasex_g['yield_missing'] = df_gasex_g['KERNELDRYWT_PERPLANT'].isna().astype(int)
 quantcols = list(df_gasex_g.columns[9:])
-plt.figure(figsize=(12,8))
-sns.heatmap(df_gasex_g.loc[df_gasex_g['YEAR'] == '2023', quantcols].corr(), annot=True, cmap='coolwarm', vmin=-1, vmax=1)
-plt.show()
+st.header("Missingness correlation with trait values")
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.heatmap(df_gasex_g.loc[df_gasex_g['YEAR'] == '2023', quantcols].corr(), annot=True, cmap='coolwarm', vmin=-1, vmax=1,
+            ax=ax)
+st.pyplot(fig)
 quantcols.remove('yield_missing')
 df_gasex_g.drop(columns = 'yield_missing', inplace = True)
 
+df_gasex_g.sort_values(by='YEAR', inplace=True)
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.heatmap(df_gasex_g.isna(), cmap="magma", ax = ax)
+st.pyplot(fig)
+st.markdown('All the missing yield is from 2023 and does not appear to be related to other traits so let us drop missing')
+cols2keep = quantcols
+cols2keep.append('YEAR')
+df_gasex_g = df_gasex_g.dropna()
+
+
 # It looks like kernel weight might have outlier, let's scale and see how many sd 
-numeric_cols = df_gasex_g.select_dtypes(include=[np.number]).columns
+numeric_cols = ['KERNELDRYWT_PERPLANT', 'DAYSTOSILK', 'AVGFLAGHT_CM']
 df_g_s = df_gasex_g[numeric_cols].apply(zscore)
 
 # looks like one point is almost 5 SD above while everything else is > 3, let's remove
-sns.scatterplot(data=df_g_s, x= df_g_s.index, y = 'KERNELDRYWT_PERPLANT')
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.scatterplot(data=df_g_s, x= df_g_s.index, y = 'KERNELDRYWT_PERPLANT', ax=ax)
 plt.title('Yield ZScores Before')
-plt.show()
+st.pyplot(fig)
+st.markdown('Remove the point with a z-score about 5')
 df_gasex_g = df_gasex_g.loc[df_gasex_g['PLOT_YEAR'] != '7318_2023']
-
 df_g_s = df_gasex_g[numeric_cols].apply(zscore)
 
 # After
-sns.scatterplot(data=df_g_s, x= df_g_s.index, y = 'KERNELDRYWT_PERPLANT')
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.scatterplot(data=df_g_s, x= df_g_s.index, y = 'KERNELDRYWT_PERPLANT', ax = ax)
 plt.title('Yield ZScores After')
-plt.show()
+st.pyplot(fig)
+
+#Silking
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.scatterplot(data=df_g_s, x= df_g_s.index, y = 'DAYSTOSILK', ax=ax)
+plt.title('Silking ZScores')
+st.pyplot(fig)
+st.markdown('Looks good, you can see difference between years but that is not rare')
+
+# Height
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.scatterplot(data=df_g_s, x= df_g_s.index, y = 'AVGFLAGHT_CM', ax=ax)
+plt.title('Height ZScores')
+st.pyplot(fig)
+st.markdown('Looks good!')
 
 # Plot_Years of interest
 plots2keep = set(df_gasex_g['PLOT_YEAR'].values)
@@ -117,7 +135,7 @@ keys = ['PLOT_YEAR', 'PLOT', 'YEAR']
 # select only those plots we are interested in
 df_ref = df_ref.loc[df_ref['PLOT_YEAR'].isin(plots2keep)]
 
-print(df_ref.isna().sum())
+
 # The multispectral reflectance data is most useful when processed into vegetative indices 
 # Let's make MS table now
 # Prepare data
@@ -142,5 +160,10 @@ df_ref = pd.merge(df_meta, unmelt, on =['PLOT', 'YEAR'], how = 'inner')
 df_ref = df_ref.loc[df_ref['PLOT_YEAR'].isin(plots2keep), ]
 df_ref['DAP'] = df_ref['DAP'].astype('int')
 
-sns.lineplot(data=df_ref, x = 'DAP', y = 'NDVI', hue = 'YEAR')
-plt.show()
+st.subheader('Remote Sensing')
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.lineplot(data=df_ref, x = 'DAP', y = 'NDVI', hue = 'YEAR', ax=ax)
+st.pyplot(fig)
+
+st.markdown('Remote sensing started earlier in 2022 and endedd later in 2023 but otherwise there are no missing values.')
+
