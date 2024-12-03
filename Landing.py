c9cfb2c443e09cab88bf2fc2a0f495b8ad0103ca
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-12-02 19:49:16 -0500

    Update Landing.py

--- Landing.py
+++ Landing.py
@@ -6,7 +6,7 @@ In recent years there has been an explosion of the data types available to plant
 hard to manage. Multi-omic Mangement aims to provide a resource that can help integrate and leverage multi-omic data in a plant breeding context.'''
 st.markdown(md)
 
-st.title('Data Types:')
+st.title('Data Types and Description:')
 st.subheader('1. Agronomic')
 col1, col2 = st.columns(2)
 with col1:
@@ -24,11 +24,29 @@ with col1:
     st.image('Supp/LeafGasEx.png', caption = 'Gas exchange through leaf stomata')
 
 with col2:
-    st.header("Gas exchange parameters")
-    st.markdown("* A - CO2 assimilation rate (µmol CO2 m⁻² s⁻¹)")
-    st.markdown("* E - Transpiration of H2O (mol H2O m⁻² s⁻¹)")
-    st.markdown("* gsw - stomatal conductance to H2O (mol H2O m⁻² s⁻¹)")
-    st.markdown("* Ci - interceullar CO2 concentration ready for assimilation (ppm)")
+    st.markdown('Gas ex traits measure how much CO2 and oxygen plants are "Breathing" which is in turn related to their metabolism.')
+    st.markdown("**A** - CO2 assimilation rate (µmol CO2 m⁻² s⁻¹)")
+    st.markdown("**E** - Transpiration of H2O (mol H2O m⁻² s⁻¹)")
+    st.markdown("**gsw** - stomatal conductance to H2O (mol H2O m⁻² s⁻¹)")
+    st.markdown("**Ci** - interceullar CO2 concentration ready for assimilation (ppm)")
 
-st.subheader('Gas exchange can be measured by a Licor')
-st.image('Supp/Licor.PNG', caption = 'Licor 6800 measuring plant leaf in the field')
+st.image('Supp/Licor.PNG', caption = 'Licor 6800 measuring gas ex. in the field')
+
+st.subheader('3. Remote Sensing')
+col1, col2 = st.columns(2)
+with col1:
+    st.image('Supp/Drone.jpg', caption = 'DJI drone used to collect multispectral imagery')
+    st.image('Supp/NDVI.png', caption = 'NDVI is generally related to chlorophyll content')
+
+with col2:
+    st.header("Remote Sensing Parameters:")
+    st.markdown('''Reflectance values of red, green, blue, near-infrared(NIR), and red-edge wavelengths are together called multispectral imagery. 
+                They can be collected quickly by flying a drone over a field and then processed into vegetative indices such as NDVI. Veg indices can be
+                highly associated with other traits of interest such as yield. One advantage of remote sensing data is that it provides a nice time-series 
+                data set that can span much of the growing season.''')
+    st.markdown("**Vegetative Indices**:")
+    st.markdown("**NDVI**, **GNDVI**, **RDVI**, **NLI**, **CVI**, **MSR**, **NDI**, **NDVIRedEdge**, **PSRI**, **CIRedEdge**, **MTCI**")
+    
+
+st.image('Supp/RGBFIELD.png', caption = 'RGB image from drone')
+st.image('Supp/SAVIFIELD.JPG', caption = 'NDVI image from drone')
