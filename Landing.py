13957cf462cc312a280328cff02527db34639976
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-12-03 17:48:20 -0500

    DataExplore

--- Landing.py
+++ Landing.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Multi-omic Management for Plant Breeding')
 md = '''The plant breeding process makes extensive use of data science techniques to identify and select useful plant germplasm. 
-In recent years there has been an explosion of the data types available to plant breeders (gen _omics_ and phen _omics_). The abundance of data can be helpful but also 
+In recent years there has been an explosion of the data types available to plant breeders (gen"omics" and phen"omics" to name two). The abundance of data can be helpful but also 
 hard to manage. Multi-omic Mangement aims to provide a resource that can help integrate and leverage multi-omic data in a plant breeding context.'''
 st.markdown(' * On this page learn about the different data types')
 st.markdown(" * On _DataExplorer_ investigate variable relationships")
@@ -28,7 +28,7 @@ with col1:
     st.image('Supp/LeafGasEx.png', caption = 'Gas exchange through leaf stomata')
 
 with col2:
-    st.markdown('Gas ex traits measure how much CO2 and oxygen plants are "Breathing" which is in turn related to their metabolism.')
+    st.markdown('Gas ex traits measure how much CO2 and oxygen plants are "Breathing" which is in turn related to their metabolism and how much carbon they can assimilate.')
     st.markdown("**A** - CO2 assimilation rate (µmol CO2 m⁻² s⁻¹)")
     st.markdown("**E** - Transpiration of H2O (mol H2O m⁻² s⁻¹)")
     st.markdown("**gsw** - stomatal conductance to H2O (mol H2O m⁻² s⁻¹)")
@@ -43,7 +43,7 @@ with col1:
     st.image('Supp/NDVI.png', caption = 'NDVI is generally related to chlorophyll content')
 
 with col2:
-    st.markdown('''Reflectance values of red, green, blue, near-infrared(NIR), and red-edge wavelengths are together called multispectral imagery. 
+    st.markdown('''Reflectance values of **red**, **green**, **blue**, **near-infrared(NIR)**, and **red-edge** wavelengths are together called multispectral (MS) imagery. 
                 They can be collected quickly by flying a drone over a field and then processed into vegetative indices such as NDVI. Veg indices can be
                 highly associated with other traits of interest such as yield. One advantage of remote sensing data is that it provides a nice time-series 
                 data set that can span much of the growing season. _See_ _"DataProcessing"_ _tab for specific calcs_.''')
--- pages/1_DataExplorer.py
+++ pages/1_DataExplorer.py
@@ -6,3 +6,70 @@ from scipy.stats import zscore
 import numpy as np
 
 st.header("Explore Data")
+
+def main():
+    st.title('Agricultural Data Explorer')
+    
+    # Load data
+    df = pd.read_csv('Data/GasExAgron.csv', index = False)
+    
+    # Sidebar for selecting graph type and variables
+    st.sidebar.header('Graph Configuration')
+    
+    # Graph type selection
+    graph_type = st.sidebar.selectbox(
+        'Select Graph Type',
+        ['Scatter Plot', 'Box Plot', 'Violin Plot']
+    )
+    
+    # X-axis variable selection
+    x_var = st.sidebar.selectbox(
+        'X-Axis Variable',
+        [col for col in df.columns if col not in ['PLOT_YEAR', 'YEAR']]
+    )
+    
+    # Y-axis variable selection
+    y_var = st.sidebar.selectbox(
+        'Y-Axis Variable',
+        [col for col in df.columns if col not in ['PLOT_YEAR', 'YEAR', x_var]]
+    )
+    
+    # Color by selection
+    color_var = st.sidebar.selectbox(
+        'Color By',
+        ['NTREATMENT', 'SUBPOPULATION', 'GENOTYPE']
+    )
+    
+    # Create the plot based on selected graph type
+    if graph_type == 'Scatter Plot':
+        fig = px.scatter(
+            df, 
+            x=x_var, 
+            y=y_var, 
+            color=color_var, 
+            title=f'{x_var} vs {y_var}'
+        )
+    elif graph_type == 'Box Plot':
+        fig = px.box(
+            df, 
+            x=color_var, 
+            y=y_var, 
+            title=f'{y_var} by {color_var}'
+        )
+    else:  # Violin Plot
+        fig = px.violin(
+            df, 
+            x=color_var, 
+            y=y_var, 
+            title=f'{y_var} Distribution by {color_var}'
+        )
+    
+    # Display the plot
+    st.plotly_chart(fig, use_container_width=True)
+    
+    # Basic statistics
+    st.header('Data Summary')
+    st.dataframe(df.describe())
+
+if __name__ == '__main__':
+    main()
