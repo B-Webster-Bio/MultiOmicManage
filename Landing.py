af123a473599d4ee59f98bcdda1974defd9bd129
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-12-02 19:33:47 -0500

    Update Landing.py

--- Landing.py
+++ Landing.py
@@ -13,7 +13,22 @@ with col1:
     st.image(r'Supp/CornHarvest.PNG', caption = 'Hand corn harvest (PBS Wisconsin)')
 
 with col2:
-    st.markdown('Agronomic traits were collected by hand. They include some of the most important trait that a plant breeding might consider')
+    st.markdown('Agronomic traits were collected by hand. They include some of the most important trait that a plant breeding might consider:')
     st.markdown("**KernelDryWt_PerPlant** -  Avg grams of grain per plant harvested")
     st.markdown('**DaysToSilk** - The number of days after planting it takes for flowering to occur')
     st.markdown('**AvgFlagHt** - The height measured from the ground to the top leaf')
+
+st.subheader('2. Leaf gas exchange')
+col1, col2 = st.columns(2)
+with col1:
+    st.image('Supp/LeafGasEx.png', caption = 'Gas exchange through leaf stomata')
+
+with col2:
+    st.header("Gas exchange parameters")
+    st.markdown("* A - CO2 assimilation rate (µmol CO2 m⁻² s⁻¹)")
+    st.markdown("* E - Transpiration of H2O (mol H2O m⁻² s⁻¹)")
+    st.markdown("* gsw - stomatal conductance to H2O (mol H2O m⁻² s⁻¹)")
+    st.markdown("* Ci - interceullar CO2 concentration ready for assimilation (ppm)")
+
+st.subheader('Gas exchange can be measured by a Licor')
+st.image('Supp/Licor.PNG', caption = 'Licor 6800 measuring plant leaf in the field')
