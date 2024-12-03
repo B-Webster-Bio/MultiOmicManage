2cf53c20698ab3dad7f1d7246962622a570dbbba
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-12-02 21:23:41 -0500

    Update 4_DataProcessing2.py

--- pages/4_DataProcessing2.py
+++ pages/4_DataProcessing2.py
@@ -96,7 +96,6 @@ df_g_s = df_gasex_g[numeric_cols].apply(zscore)
 fig, ax = plt.subplots(figsize=(8, 5))
 sns.scatterplot(data=df_g_s, x= df_g_s.index, y = 'KERNELDRYWT_PERPLANT', ax=ax)
 plt.title('Yield ZScores Before')
-ax.hlines(y=3)
 st.pyplot(fig)
 
 df_gasex_g = df_gasex_g.loc[df_gasex_g['PLOT_YEAR'] != '7318_2023']
