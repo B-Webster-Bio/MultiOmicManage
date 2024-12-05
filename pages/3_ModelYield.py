a7f5af660e59c50949fe165369f70c730752f3c4
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-12-04 21:29:22 -0500

    Update 3_ModelYield.py

--- pages/3_ModelYield.py
+++ pages/3_ModelYield.py
@@ -30,7 +30,7 @@ df_all = df_all.dropna(axis=1)
 st.dataframe(df_all)
 y = df_all['KERNELDRYWT_PERPLANT']
 
-s1 = df_all.columns[6:10]
-s2 = df_all.columns[12:]
+s1 = list(df_all.columns[6:10])
+s2 = list(df_all.columns[12:])
 all_possible_features = s1 + s2
 st.write(all_possible_features)
